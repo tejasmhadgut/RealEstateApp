1d97a229dfaa80a3f06d9655cbd5de6e67ed2848
Tejas Mhadgut <tejasmhadgut@gmail.com>
2024-09-28 20:34:33 -0400

    Created Mongo Connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongooose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
+mongooose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB!');
+}).catch((err)=>{
+    console.log(err);
+});
 const app = express();
 
 app.listen(3000,()=>{
