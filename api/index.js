1ff10bc6ff345b76a4f9220fa6bf026a2cebfb4a
Tejas Mhadgut <tejasmhadgut@gmail.com>
2024-09-30 18:07:14 -0400

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongooose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
 dotenv.config()
 
 mongooose.connect(process.env.MONGO).then(()=>{
@@ -10,10 +12,11 @@ mongooose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 const app = express();
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!!!');
 })
 
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
 
     },
-    username:{
+    password:{
         type:String,
         required: true,
 
