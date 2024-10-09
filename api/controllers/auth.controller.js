15d2b7c5f6813f559feb1e77c17754fb8b55243b
Tejas Mhadgut <tejasmhadgut@gmail.com>
2024-10-09 17:04:24 -0400

    created user update api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import jwt  from "jsonwebtoken";
-import { userInfo } from "os";
+
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
